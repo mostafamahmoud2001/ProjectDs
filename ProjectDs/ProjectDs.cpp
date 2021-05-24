d175bbb12692cc0c6c0890a445b645a51d5bc54a
mostafamahmoud2001 <mostafamhmoud393@gmail.com>
2021-05-24 16:17:17 +0200

    sd

--- ProjectDs/ProjectDs.cpp
+++ ProjectDs/ProjectDs.cpp
@@ -6,12 +6,12 @@ int main()
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
+    std::cout << "Hellodadsada World!\n";
     std::cout << "Hello World!\n";
     std::cout << "Hello World!\n";
+    std::cout << "Hello Wsdsdorld!\n";
     std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
-    std::cout << "Hello World!\n";
+    std::cout << "Hello mostafa!\n";
     std::cout << "Hello World!sssss\n";
 
 }
