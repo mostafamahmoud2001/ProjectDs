b79d12c0811b2e118684f33b1589308a82af4da8
mostafamahmoud2001 <62348942+mostafamahmoud2001@users.noreply.github.com>
2021-05-28 03:18:23 +0200

    Create Product.h

---
+++ ProjectDs/Product.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include<vector>
+using namespace std;
+class Product
+{
+	int id;
+	string name;
+	float price;
+	string category;
+	int sellerId;
+	int quantity;
+public:
+	float rate;
+	vector<int>rates;
+	static int productNum;
+	Product(string name, string category, float price, int sellerId, int quantity);
+	string getcategory();
+	string getname();
+	int getid();
+	float getprice();
+	int getquantity();
+};
